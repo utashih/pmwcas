18d0adc3d7e9deafc2f0be03df1d472a1ea259b6
Xiangpeng Hao <xha62@sfu.ca>
2019-12-02 17:36:29 -0800

    fix ci

--- public/data_process.py
+++ public/data_process.py
@@ -12,14 +12,14 @@ def process():
 
         bench_data = []
         with open('bench_data') as bench:
-            bench_data = bench.readlines[0]
+            bench_data = bench.readlines()[0]
             bench_data = bench_data.split(' ')
 
         data['data'].append({
             'gitTag': build_data[0][:5],
             'buildId': int(build_data[1]),
-            'ops': int(bench_data[0]),
-            'success': int(bench_data[1])
+            'ops': float(bench_data[0]),
+            'success': float(bench_data[1])
         })
 
     with open('data.json', 'w') as f:
