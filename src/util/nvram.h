4f0bd3c7218d4391476ccfc02e5766fd8b88ed4f
Xiangpeng <me@haoxp.xyz>
2020-06-30 10:33:33 -0700

    simplify more, remove examples

--- src/util/nvram.h
+++ src/util/nvram.h
@@ -15,42 +15,6 @@ namespace pmwcas {
 
 struct NVRAM {
 #ifdef PMEM
-  /// How many cycles to delay for an emulated NVRAM write
-  static uint64_t write_delay_cycles;
-  static double write_byte_per_cycle;
-  static bool use_clflush;
-
-  static void InitializeClflush() {
-    use_clflush = true;
-    LOG(INFO) << "Will use CLFLUSH";
-  }
-
-  static void InitializeSpin(uint64_t delay_ns, bool emulate_wb) {
-    use_clflush = false;
-    if(delay_ns) {
-      uint64_t start = __rdtsc();
-      sleep(1);
-
-      uint64_t end = __rdtsc();
-      write_delay_cycles = (double)(end - start) / 1000000000 * delay_ns;
-    }
-    LOG(INFO) << "Write delay: " << delay_ns << "ns (" <<
-        write_delay_cycles << " cycles)";
-
-    if(emulate_wb) {
-      char test_array[kCacheLineSize];
-      unsigned int not_used = 0;
-      uint64_t start = __rdtscp(&not_used);
-      _mm_clflush(test_array);
-      uint64_t end = __rdtscp(&not_used);
-      write_byte_per_cycle = (double)kCacheLineSize / (end - start);
-    } else {
-      write_byte_per_cycle = 0;
-    }
-
-    LOG(INFO) << "BW emulation: " << write_byte_per_cycle << " bytes per cycle";
-  }
-
   static inline void Flush(uint64_t bytes, const void* data) {
 #ifdef PMDK
     auto pmdk_allocator = reinterpret_cast<PMDKAllocator*>(Allocator::Get());
@@ -64,23 +28,6 @@ struct NVRAM {
         _mm_clflush(&((char*)data)[i * ncachelines]);
       }
     } else {
-#if 0
-      // Previously this was calculated outside the [if] block, slowing down
-      // read-only workloads (slower than with clflush).
-      //
-      // Update #2: perhaps the fairest way is comment out this whole else
-      // block so it does nothing when we don't specify delays.
-      if(write_delay_cycles > 0) {
-        unsigned int aux = 0;
-        uint64_t bw_cycles = write_byte_per_cycle > 0 ?
-          bytes / write_byte_per_cycle : 0;
-        uint64_t start = __rdtscp(&aux);
-
-        while (__rdtscp(&aux) - start < write_delay_cycles + bw_cycles) {
-          // SPIN
-        }
-      }
-#endif
     }
 #endif  // PMDK
   }
